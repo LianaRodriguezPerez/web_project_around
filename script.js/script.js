9a16861d31b28c7df6cef49842d93e50037ec8b0
LianaRodriguezPerez <lemanerp@gmail.com>
2024-08-07 19:59:19 -0600

    correcciòn js

--- script.js/script.js
+++ script.js/script.js
@@ -1,7 +1,9 @@
+    
+    // Editar y cerrar perfil
+    
     let editar=document.querySelector('.content__profile-button');
     let ventana=document.querySelector('.popup');
     let cerrar=document.querySelector('.popup__container-close');
-    let formElement=document.querySelector('.popup__container-formulario')
 
     editar.addEventListener('click', function(){
         ventana.classList.add('active');
@@ -11,4 +13,33 @@
         ventana.classList.remove('active');
 
     });
+    // cerrar perfil despues de guardar
+    function savePopUp() {
+        ventana.classList.remove('active');
+      }
+
+     // Guardar perfil
+     let formElement=document.querySelector('.popup__container-formulario');
+     let profileName=document.querySelector('.content__profile-name');
+     let profileAbout=document.querySelector('.content__profile-parrafo');
+     let nameInput=document.querySelector('.popup__container-name');
+     let jobInput=document.querySelector('.popup__container-about');
+
+     function handleProfileFormSubmit(evt) {
+        evt.preventDefault();
+
+        let nameValue = nameInput.value;
+        let jobValue = jobInput.value;
+
+        if(nameValue && jobValue){
+            profileName.textContent= nameValue;
+            profileAbout.textContent=jobValue;
+            formElement.reset();
+            savePopUp()
+        }
+    }
+    formElement.addEventListener("submit", handleProfileFormSubmit);
+
+
+
 
